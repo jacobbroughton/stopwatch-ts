688b1fbb73299def864f8cea3d5ea316f1b4bd89
Jacob <jlbroughton88@gmail.com>
2025-04-08 17:46:18 -0400

    initial commit, stripped vite defaults

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stopwatch
+<p>A stopwatch built in React</p>
+<p>This is primarily for practicing asynchronous behavior</p>
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Stopwatch</p>
+    </>
+  );
+}
+
+export default App;
